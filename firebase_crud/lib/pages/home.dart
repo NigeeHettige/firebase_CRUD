c92fc57792bebc06e5629091a9de5595c20961f0
Nigee Hettige <nigeehettige20@gmail.com>
2024-03-08 00:28:34 +0530

    modification_2

---
+++ firebase_crud/lib/pages/home.dart
@@ -0,0 +1,54 @@
+import 'package:firebase_crud/pages/employee.dart';
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => Employee()),
+          );
+        },
+        child: const Icon(Icons.add),
+      ),
+      appBar: AppBar(
+        title: const Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Flutter',
+              style: TextStyle(
+                color: Colors.blue,
+                fontSize: 24.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              'Firebase',
+              style: TextStyle(
+                color: Color.fromARGB(255, 243, 227, 83),
+                fontSize: 24.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+      ),
+      body: Container(
+        child: const Column(
+          children: [],
+        ),
+      ),
+    );
+  }
+}
